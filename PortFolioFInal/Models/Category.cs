d09e432df8051d5867ed461db8653525164a6088
Niloy Das <niloyokasd@gmail.com>
2024-03-03 17:00:00 +0600

    Add Category and Login models

---
+++ PortFolioFInal/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortFolioFInal.Models
+{
+    public class Category
+    {
+        public int id { get; set; }
+        [Required]
+        [MaxLength(5)]
+        [Display(Name = "Category Name")]
+        public String Name { get; set; }
+        [Display(Name = "Category Description")]
+        [Range(1 , 10 , ErrorMessage = "Display 1 to 10")]
+        public String Description { get; set; }
+    }
+}
---
+++ PortFolioFInal/Models/Login.cs
@@ -0,0 +1,10 @@
+﻿namespace PortFolioFInal.Models
+{
+    public class Login
+    {
+        public int id { get; set; }
+        public String Email { get; set; }
+        public String title { get; set; }
+        public String details { get; set;}
+    }
+}
