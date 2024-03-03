7542d7fd97f3a23ebe6842eafc23b9414b2ab862
Niloy Das <niloyokasd@gmail.com>
2024-03-03 17:20:00 +0600

    Add contact form and view models

---
+++ PortFolioFInal/Models/ContactModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortFolioFInal.Models
+{
+    
+
+    public class ContactModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Message is required")]
+        public string Message { get; set; }
+    }
+}
---
+++ PortFolioFInal/Models/Home.cs
@@ -0,0 +1,12 @@
+﻿namespace PortFolioFInal.Models
+{
+    public class Home
+    {
+        public List<Project> Projects { get; set; }
+        public List<Skill> Skills { get; set; }
+
+        public ContactModel ContactForm { get; set; } = new ContactModel();
+        public LoginModel LoginModel { get; set; } = new LoginModel();
+
+    }
+}
