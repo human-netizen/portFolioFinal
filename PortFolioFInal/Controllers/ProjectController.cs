acdef4539c2242e1fe616e2be6aa28508669c69b
Niloy Das <niloyokasd@gmail.com>
2024-03-04 12:00:00 +0600

    Add project and category management

---
+++ PortFolioFInal/Controllers/ProjectController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using PortFolioFInal.Data;
+using PortFolioFInal.Models;
+
+namespace PortFolioFInal.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProjectController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Project> objProjectList = _db.Projects.ToList();
+            return View(objProjectList);
+        }
+
+      
+        [HttpPost]
+        public IActionResult UpdateList(List<Project> projects)
+        {
+
+            var allProjects = _db.Projects.ToList();
+            _db.Projects.RemoveRange(allProjects);
+            _db.SaveChanges();
+            foreach (var project in projects)
+            {
+                var existingProject = _db.Projects.Find(project.id);
+
+                _db.Projects.Add(project);              
+
+                _db.SaveChanges();
+            }
+
+
+            // Redirect or return a response as needed
+            return RedirectToAction("Index");
+        }
+
+    }
+}
